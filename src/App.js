5fdf8c26768a0ac2eb3362565ca2e226a00c03da
Alex Martin <atonymartin20@yahoo.com>
2020-11-07 16:35:07 -0600

    App routing added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Homepage from "./components/homepage";
+import Menu from "./components/menu";
+import Contact from "./components/contact";
+import OrderNow from "./components/orderNow";
+import { AppContext } from "./components/context/appContext.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Switch>
+					<Route exact path='/'>
+						<Homepage />	
+					</Route>
+					<Route path='/menu'>
+						<Menu />
+					</Route>
+					<Route path ='/contact'>
+						<Contact />
+					</Route>
+					<Route path='/orderNow'>
+						<OrderNow />
+					</Route>
+					<Route>
+						<Homepage />
+					</Route>
+				</Switch>
+			</div>
+		)
+	}
 }
 
+App.contextType = AppContext;
 export default App;
