575db8fb851ebb53c11ee2d7e181e1ae649462b3
Alex Martin <atonymartin20@yahoo.com>
2020-11-07 10:29:23 -0600

    Added project dependencies, title, favicon, prettier formatting file, css reset, and index.js update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import AppProvider from "./components/context/appContext.js";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<AppProvider>
+		<React.StrictMode>
+			<Router>
+				<App />
+			</Router>
+		</React.StrictMode>
+	</AppProvider>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
