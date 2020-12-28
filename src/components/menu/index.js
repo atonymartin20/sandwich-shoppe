b1916e3ccd7fffb77848012c5e2464ddccc8f555
Alex Martin <atonymartin20@yahoo.com>
2020-12-27 21:40:43 -0600

    Adding layout for Inside Divs

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -2,14 +2,92 @@ import React from 'react';
 import Navbar from '../navbar';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { AppContext } from '../context/appContext.js';
+import Sandwich from '../../images/homepage-sub.jpg';
 
 const styles = theme => ({
+    groupDivDrinks: {
+        width: '45%',
+        border: '1px solid black',
+        borderRadius: 5,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        minHeight: 200,
+        fontSize: '2.5rem',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        [theme.breakpoints.down(550)]: {
+            width: '100%',
+        }
+    },
+    groupDivSalads: {
+        width: '45%',
+        border: '1px solid black',
+        borderRadius: 5,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        minHeight: 200,
+        fontSize: '2.5rem',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        [theme.breakpoints.down(550)]: {
+            width: '100%',
+        }
+    },
+    groupDivSandwich: {
+        width: '45%',
+        border: '1px solid black',
+        borderRadius: 5,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        minHeight: 200,
+        fontSize: '2.5rem',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${Sandwich})`,
+        [theme.breakpoints.down(550)]: {
+            width: '100%',
+        }
+    },
+    groupDivSides: {
+        width: '45%',
+        border: '1px solid black',
+        borderRadius: 5,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        minHeight: 200,
+        fontSize: '2.5rem',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        [theme.breakpoints.down(550)]: {
+            width: '100%',
+        }
+    },
     menuDiv: {
         width: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
+    },
+    menuInsideContainer: {
+        width: '100%',
+        maxWidth: 1400,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
     },
     menuSpacingDiv: {
         marginTop: 90,
@@ -41,11 +119,15 @@ class Menu extends React.Component {
                 <Navbar />
                 <div className={classes.menuSpacingDiv}>
                     <h1>Menu:</h1>
-                    <div className={classes.menuInsideContiner}>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
+                    <div className={classes.menuInsideContainer}>
+                        <div className={classes.groupDivSandwich}>
+                            <div className={classes.insideGroupDiv}>
+                                Sandwiches
+                            </div>
+                        </div>
+                        <div className={classes.groupDivSalads}>Salads</div>
+                        <div className={classes.groupDivSides}>Sides</div>
+                        <div className={classes.groupDivDrinks}>Drinks</div>
                     </div>
                 </div>
             </div>
