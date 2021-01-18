f5d1b84432e84c5e75860e940dac06387e368626
Alex Martin <atonymartin20@yahoo.com>
2021-01-18 15:52:01 -0600

    finished sandwichSizeRadioButtons

--- src/components/menu/sandwich/sandwichSizeRadioButtons.js
+++ src/components/menu/sandwich/sandwichSizeRadioButtons.js
@@ -7,13 +7,22 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+    formControlLabel: {
+        margin: '0px 10px',
+    },
     label: {
         fontSize: '2.0rem',
+        padding: '0px 10px 0px 0px',
     },
     radioGroup: {
         display: 'flex',
         width: '100%',
         flexDirection: 'row',
+        padding: '10px 30px',
+        justifyContent: 'space-evenly',
+        [theme.breakpoints.down(550)]: {
+            padding: '10px 10px',
+        }
     },
     root: {
         width: '100%',
@@ -23,14 +32,23 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-export default function SandwichSizeRadioButtons() {
+export default function SandwichSizeRadioButtons(props) {
     const classes = useStyles();
 	const [value, setValue] = React.useState('small');
 
 	const handleChange = (event) => {
-		setValue(event.target.value);
-	};
-
+        setValue(event.target.value);
+        if(event.target.value === 'small') {
+            props.updateButtons(true, false, false)
+        }
+        else if(event.target.value === 'medium') {
+            props.updateButtons(false, true, false)
+        }
+        else if(event.target.value === 'large') {
+            props.updateButtons(false, false, true)
+        }
+    };
+    
 	return (
 		<FormControl component="fieldset" className={classes.root}>
 			<RadioGroup
@@ -43,17 +61,20 @@ export default function SandwichSizeRadioButtons() {
 				<FormControlLabel
 					value="small"
 					control={<Radio color="default" />}
-                    label= {<Typography className={classes.label}>Small</Typography>}
+                    label= {<Typography className={classes.label}>Small Sandwich ${props.price}</Typography>}
+                    className={classes.formControlLabel}
 				/>
 				<FormControlLabel
 					value="medium"
 					control={<Radio color="default" />}
-                    label= {<Typography className={classes.label}>Medium</Typography>}
+                    label= {<Typography className={classes.label}>Medium Sandwich ${props.price + 2}</Typography>}
+                    className={classes.formControlLabel}
 				/>
 				<FormControlLabel
 					value="large"
 					control={<Radio color="default" />}
-                    label= {<Typography className={classes.label}>Large</Typography>}
+                    label= {<Typography className={classes.label}>Large Sandwich ${props.price + 4}</Typography>}
+                    className={classes.formControlLabel}
 				/>
 			</RadioGroup>
 		</FormControl>
