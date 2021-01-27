aed2b1117c7d86ee04e73840de80e1bc2940aaa5
Alex Martin <atonymartin20@yahoo.com>
2021-01-27 10:59:45 -0600

    Created and Completed Sides Component

--- src/components/menu/sides/sides.js
+++ src/components/menu/sides/sides.js
@@ -5,8 +5,8 @@ import CheesebreadImg from '../../../images/Sides/Cheesebread.jpg';
 import ChipsImg from '../../../images/Sides/Chips.jpg';
 import CookiesImg from '../../../images/Sides/Cookies.jpg';
 import Button from "@material-ui/core/Button";
-import { Redirect } from 'react-router-dom';
 import SideInfo from './sideinfo.js';
+import CreateSide from './createSide.js';
 
 const styles = theme => ({
     backButton: {
@@ -152,14 +152,14 @@ const styles = theme => ({
         textAlign: 'center',
         cursor: 'pointer',
     },
-    menuDiv: {
+    sideDiv: {
         width: '100%',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
     },
-    menuInsideContainer: {
+    sideInsideContainer: {
         width: '100%',
         maxWidth: 1400,
         display: 'flex',
@@ -167,7 +167,7 @@ const styles = theme => ({
         justifyContent: 'center',
         flexWrap: 'wrap',
     },
-    menuSpacingDiv: {
+    sideSpacingDiv: {
         marginTop: 90,
         width: '100%',
         maxWidth: 1400,
@@ -182,7 +182,7 @@ const styles = theme => ({
         fontSize: '2.0rem',
         lineHeight: 1.25,
     },
-    menuSpacingOrderDiv: {
+    sideSpacingOrderDiv: {
         marginTop: 0,
         width: '100%',
         maxWidth: 1400,
@@ -197,25 +197,23 @@ const styles = theme => ({
         fontSize: '2.0rem',
         lineHeight: 1.25,
     },
-    menuText: {
-        marginTop: 20,
-        marginBottom: 20,
-    },
 });
 
 class Sides extends React.Component {
     state={
-        redirect: false,
         moreInfoCheesebread: false,
         moreInfoChips: false,
         moreInfoCookies: false,
-    }
-
-    checkout = (event) => {
-        event.preventDefault();
-        this.setState({
-            redirect: true,
-        })
+        createSide: false,
+        side: {
+            name: '',
+            type: '',
+            price: 0.00,
+            is1CookiePack: false,
+            is3CookiePack: false,
+            is5CookiePack: false,
+            addMarinara: false,
+        }
     }
 
     openMoreInfoCheesebread = (event) => {
@@ -248,19 +246,55 @@ class Sides extends React.Component {
         })
     }
 
+    createSide = (side) => {
+        let newSide = this.state.side;
+        Object.keys(newSide).forEach(function (newSideKey) {
+            Object.keys(side).forEach(function (sideKey) {
+                if(sideKey === newSideKey) {
+                    newSide[newSideKey] = side[sideKey]
+                }
+            })
+        });
+
+        this.setState({
+            side: newSide,
+            createSide: true,
+        })
+    }
+
+    closeMenu = (event) => {
+        event.preventDefault();
+        let side = {
+            name: '',
+            type: '',
+            price: 0.00,
+            is1CookiePack: false,
+            is3CookiePack: false,
+            is5CookiePack: false,
+            addMarinara: false,
+        }
+
+        this.setState({
+            createSide: false,
+            side,
+        })
+    }
+
     render() {
         const { classes } = this.props;
+
         if(this.props.type === 'order') {
             return (
-                <div className={classes.menuDiv}>
-                    {this.state.redirect === true ? <Redirect to='/orderNow/checkout' /> : null }
-                    <div className={classes.menuSpacingOrderDiv}>
+                <div className={classes.sideDiv}>
+                    {this.state.createSide === true ? <CreateSide itemAddedToCart={this.props.itemAddedToCart} title={this.state.side['type']} side={this.state.side} close={this.closeMenu} /> : null }
+
+                    <div className={classes.sideSpacingOrderDiv}>
                         {this.context.state.orderItemCount > 0 ? 
                             <div className={classes.buttonContainerDiv}>
                                 <Button className={classes.backButton} onClick={this.props.goBack}>
                                     Go Back
                                 </Button>
-                                <Button className={classes.checkoutButton} onClick={this.checkout}>
+                                <Button className={classes.checkoutButton} onClick={this.props.goToCheckout}>
                                     Checkout
                                 </Button>
                             </div> : 
@@ -269,21 +303,21 @@ class Sides extends React.Component {
                             </div>
                         }
                         <h1 className={classes.headerText}>Sides</h1>
-                        <div className={classes.menuInsideContainer}>
+                        <div className={classes.sideInsideContainer}>
     
-                            <div className={classes.groupDivCheesebread} onClick={this.chooseSandwiches}>
+                            <div className={classes.groupDivCheesebread} onClick={() => {this.createSide({type: 'Cheesebread', name: 'Cheesebread', price: 4.49})}}>
                                 <div className={classes.insideGroupDiv}>
                                     Cheesebread
                                 </div>
                             </div>
     
-                            <div className={classes.groupDivChips} onClick={this.chooseSandwiches}>
+                            <div className={classes.groupDivChips} onClick={() => {this.createSide({type: 'Chips', price: 1.09})}}>
                                 <div className={classes.insideGroupDiv}>
                                     Chips
                                 </div>
                             </div>
     
-                            <div className={classes.groupDivCookies} onClick={this.chooseSandwiches}>
+                            <div className={classes.groupDivCookies} onClick={() => {this.createSide({type: 'Cookies', price: 0.00})}}>
                                 <div className={classes.insideGroupDiv}>
                                     Cookies
                                 </div>
@@ -295,17 +329,18 @@ class Sides extends React.Component {
         }
         else {
             return (
-                <div className={classes.menuDiv}>
+                <div className={classes.sideDiv}>
+                    <div className={classes.sideSpacingDiv}>
+                        {this.state.moreInfoCheesebread === true ? <SideInfo close={this.closeMoreInfo} name='Cheesebread' text='Stuffed cheesebread with garlic and italian seasoning' img={CheesebreadImg} /> : null }
+                        {this.state.moreInfoChips === true ? <SideInfo close={this.closeMoreInfo} name='Chips' text='Doritos, Fritos, Lays, or Sun Chips' img={ChipsImg} /> : null }
+                        {this.state.moreInfoCookies === true ? <SideInfo close={this.closeMoreInfo} name='Cookies' text='Chocolate chip or sugar cookies' img={CookiesImg} /> : null }
 
-                    {this.state.moreInfoCheesebread === true ? <SideInfo close={this.closeMoreInfo} name='Cheesebread' text='Stuffed cheesebread with garlic and italian seasoning' img={CheesebreadImg} /> : null }
-                    {this.state.moreInfoChips === true ? <SideInfo close={this.closeMoreInfo} name='Chips' text='Doritos, Fritos, Lays, or Sun Chips' img={ChipsImg} /> : null }
-                    {this.state.moreInfoCookies === true ? <SideInfo close={this.closeMoreInfo} name='Cookies' text='Chocolate chip or sugar cookies' img={CookiesImg} /> : null }
-                    <div className={classes.menuSpacingDiv}>
                         <div className={classes.goBackDiv}>
                             <Button className={classes.backButton} onClick={this.props.goBack}>Go Back</Button>
                         </div>
+
                         <h1 className={classes.headerText}>Sides</h1>
-                        <div className={classes.menuInsideContainer}>
+                        <div className={classes.sideInsideContainer}>
     
                             <div className={classes.groupDivCheesebread} onClick={this.openMoreInfoCheesebread}>
                                 <div className={classes.insideGroupDiv}>
