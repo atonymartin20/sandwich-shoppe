1c9cff4764bf613b76307433037784aa1867844d
Alex Martin <atonymartin20@yahoo.com>
2021-02-01 23:56:08 -0600

    Homepage completed

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -2,8 +2,59 @@ import React from 'react';
 import Navbar from '../navbar';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { AppContext } from '../context/appContext.js';
+import BuffaloChickenImg from '../../images/Sandwiches/BuffaloChicken.jpg';
+import Button from "@material-ui/core/Button";
+import { Link } from 'react-router-dom';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import Socials from './socialsContainer.js';
 
 const styles = theme => ({
+    button: {
+        backgroundColor: '#7600A8',
+        marginTop: '2%',
+        fontWeight: 500,
+        height: 50,
+        fontSize: '2.5rem',
+        color: 'white',
+        padding: '0px 15px',
+        border: '3px solid #7600A8',
+        borderRadius: '10px',
+        width: '200px',
+        textTransform: 'none',
+        "&:hover": {
+            color: '#7600A8',
+            backgroundColor: 'white',
+        },
+        [theme.breakpoints.down(350)]: {
+            width: '100%',
+            marginBottom: '10px',
+        },
+    },
+    footer: {
+        fontSize: '1.5rem',
+        backgroundColor: '#cdcdcd',
+        width: '100%',
+        minHeight: '50px',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerLargeText: {
+        fontSize: '8.0rem',
+        [theme.breakpoints.down(400)]: {
+            fontSize: '7.0rem',
+        },
+    },
+    headerMainText: {
+        fontSize: '4.5rem',
+        marginTop: '2%',
+        [theme.breakpoints.down(400)]: {
+            fontSize: '3.5rem',
+        },
+    },
     homepageDiv: {
         width: '100%',
         display: 'flex',
@@ -22,13 +73,53 @@ const styles = theme => ({
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-        padding: 20,
         fontSize: '2.0rem',
         lineHeight: 1.25,
     },
     homepageText: {
         marginTop: 20,
         marginBottom: 20,
+    },
+    insideImageDiv: {
+        backgroundColor: 'rgba(43, 43, 43, 0.1)',
+        width: '100%',
+        height: '75vh',
+        textShadow: '1px 1px 2px black, 1px 0px 2px black, 0px 1px 2px black, 0 0 8px #333',
+        fontSize: '4.0rem',
+        color: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        cursor: 'pointer',
+        flexDirection: 'column',
+        [theme.breakpoints.down(800)]: {
+            height: '60vh',
+        },
+        [theme.breakpoints.down(600)]: {
+            minHeight: '50vh',
+            height: 'auto',
+        },
+    },
+    mainImageDiv: {
+        width: '100%',
+        minWidth: 200,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '75vh',
+        fontSize: '2.5rem',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url(${BuffaloChickenImg})`,
+        [theme.breakpoints.down(800)]: {
+            height: '60vh',
+        },
+        [theme.breakpoints.down(600)]: {
+            minHeight: '50vh',
+            height: 'auto',
+        },
     },
 });
 
@@ -40,6 +131,22 @@ class Homepage extends React.Component {
             <div className={classes.homepageDiv}>
                 <Navbar />
                 <div className={classes.homepageSpacingDiv}>
+                    <div className={classes.mainImageDiv}>
+                        <div className={classes.insideImageDiv}>
+                            <span className={classes.headerLargeText}>Hungry?</span>
+
+                            <span className={classes.headerMainText}>We can fix that!</span>
+
+                            <Button className={classes.button} component={Link} to='/orderNow'>Order Now</Button>
+                        </div>
+                    </div>
+
+                    <Socials />
+
+                    <footer className={classes.footer}>
+                        Sandwich Shoppe is a Registered Trademark of Sandwich Shoppe Incorporated.  All Rights Reserved.
+                    </footer>
+
                 </div>
             </div>
         )
